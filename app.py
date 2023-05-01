cc29a284aa818404198feb46a3b57292c82256ed
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-05-01 22:35:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask , render_template,request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl' ,'rb'))
+
+@app.route('/')
+def hello():
+    return render_template("index1.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    #for putting the details on the html GUI
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+
+    return render_template('index1.html', prediction_text='Person should have minimum insurance of : USD {}'.format(output))
+
+
+
+if __name__=="__main__":
+    app.run()
